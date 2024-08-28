68b2725e65a3f6bb6eae0b29115fa2193dad60d4
kinseychristensen <kit.the.avenger@gmail.com>
2024-08-28 10:00:30 -0400

    improved logos and banners created.  fighting with css on app.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,16 @@
 <template>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <div id="capstone-app">
-    <div id="top-nav">NAV</div>
-    <img src="src/assets/informative_banner_logo.png"/>
+   
+    <div id="banner">
+      <div>white space</div>
+    <img  id="logo" src="src/assets/desktop_pixel_logo.png" alt="inFormative Logo"/>
+  </div>
      
   
     <router-view id="r-view"/>
+    
     <div id="nav">
       
       <router-link v-bind:to="{ name: 'home' }">Contact Us</router-link>&nbsp;&nbsp;
@@ -23,7 +28,8 @@
   background-color: #f8f9fa;
   padding: 10px;
   border-bottom: 1px solid #dee2e6;
-  grid-area: bottom-nav;
+  grid-area: nav;
+ 
 
 }
 
@@ -40,17 +46,30 @@ div{
   font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
 }
 #capstone-app{
- display: grid;
- grid-template-columns: auto;
- grid-template-areas: 
- "nav logo"
- "router router"
- "bottom-nav bottom-nav"
- ;
+  width: 100%;
   padding: 5px;
-
+  display: grid;
+  grid-template-areas: 
+  "banner"
+  "nav-bar"
+    "r-view"
+    "nav"
+;
 }
 #r-view{
-  grid-area: router;
+ padding: 2px;
+ grid-area: r-view;
 }
+
+#banner{
+  
+  background-color: white;
+
+grid-area: banner;
+display: flexbox;
+justify-content: space-between;
+flex-direction: row;
+justify-self: stretch;
+}
+
 </style>

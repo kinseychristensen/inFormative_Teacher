b8898451e462c6d0437992c97bf38d4846c82d57
kinseychristensen <kit.the.avenger@gmail.com>
2024-09-15 16:28:33 -0400

    deploying to heroku

--- Java/database/create.sh
+++ Java/database/create.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-export PGPASSWORD='postgres1'
+
+# Get the DATABASE_URL from Heroku (This pulls the URL dynamically for your app)
+HEROKU_APP_NAME="informative-teacher"  # Set your Heroku app name here
+DATABASE_URL=$(heroku config:get DATABASE_URL -a $HEROKU_APP_NAME)
+
+# Split the DATABASE_URL into its components (user, host, etc.)
+PGPASSWORD=$(echo $DATABASE_URL | sed -e 's/^.*:\(.*\)@.*$/\1/')
+PGUSER=$(echo $DATABASE_URL | sed -e 's/^.*\/\/\(.*\):.*@.*$/\1/')
+PGHOST=$(echo $DATABASE_URL | sed -e 's/^.*@\(.*\):.*$/\1/')
+PGDATABASE=$(echo $DATABASE_URL | sed -e 's/^.*\/\(.*\)$/\1/')
+
 BASEDIR=$(dirname $0)
-DATABASE=twisdale_databook
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
-createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+
+# Run SQL scripts (schema, data, and users)
+psql $DATABASE_URL -f "$BASEDIR/schema.sql" &&
+psql $DATABASE_URL -f "$BASEDIR/data.sql" &&
+psql $DATABASE_URL -f "$BASEDIR/user.sql"
